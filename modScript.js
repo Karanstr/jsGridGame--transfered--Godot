1c9e0e72add49a986c8ffc906028dec1a1493c83
Karanstr <139508165+Karanstr@users.noreply.github.com>
2024-04-10 11:29:35 +0000

    Fixed function names

--- modScript.js
+++ modScript.js
@@ -8,10 +8,10 @@ window.Vector = Vector;
 
 window.a = new Region(125, 125, 160, 160, new blockMap('#ffffff'));
 window.a.blockMap.addBlock(1, '#777777', 1);
-window.a.Load('8401 8411 8421 8441 8451 8481 84a1 8501 8511 8541 8571 85d1 85f1 8601 8621 8661 8671 86e1 86f1 8721 8731 8751 8761 8771 87a1 87b1 81f1')
+window.a.Load('8401 8411 8421 8441 8451 8481 84a1 8501 8511 8541 8571 85d1 85f1 8601 8621 8661 8671 86e1 86f1 8721 8731 8751 8761 8771 87a1 87b1 81f1');
 window.a.findCorners()
 
-window.p = new Region(150, 240, 20, 20, new blockMap('#ffffff'));
+window.p = new Region(145, 185, 20, 20, new blockMap('#ffffff'));
 window.p.blockMap.addBlock(1, '#dd7777', 1);
 window.p.Load('8011')
 window.p.findCorners()
@@ -67,8 +67,8 @@ onkeyup = (event) => { keyRemove.add(event.key) }
 window.speed = 1;
 window.gravity = new Vector(0, 0, 1)
 const game = setInterval(() => {
-  let debugCheck = document.getElementById("debug").checked
-  if (debugCheck == true) {a.debugToggle = 1} else {a.debugToggle = 0}
+  /*let debugCheck = document.getElementById("debug").checked
+  if (debugCheck == true) {a.debugToggle = 1} else { a.debugToggle = 0 }*/
   Render.drawBox(new Vector(0, 0, 0), new Vector(canData.right, canData.bottom, 1), 'white')
   if (keyMove.has('w') && keyMove.has('s')) { }
   else if (keyMove.has('w')) { currentMove.velocity.add(new Vector(0, -window.speed), true) }
@@ -84,10 +84,7 @@ const game = setInterval(() => {
     object.Render();
     object.drawCorners();
   })
-  p.checkAllCollisions(p.position, p.velocity, a);
-  p.updatePos(p.velocity);
-  p.velocity = new Vector(0, 0, 1)
-  a.updatePos(a.velocity);
-  a.velocity = new Vector(0, 0, 1)
+  p.moveWithCollisions(a)
+  p.velocity = new Vector(0, 0, 1);
 
 }, 1000 / 60);
