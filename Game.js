79e80de0418009e8ea53b6a101688d37d75cf50c
Karanstr <reynacalterion@gmail.com>
2024-06-26 14:33:53 -0400

    Converted player to a single point for convenience of testing

--- Game.js
+++ Game.js
@@ -1,29 +1,31 @@
 "use strict";
 
 import Render from './modules/Render.js';
+window.Render = Render;
 import Vector2 from './modules/Vector2.js';
+window.Vector2 = Vector2;
 import WorldObject from './modules/WorldObject.js';
+window.WorldObject = WorldObject;
+import Physics from './modules/Physics.js'
+window.Physics = Physics
+
 const canvas = document.getElementById("canvas");
 let canData = canvas.getBoundingClientRect();
 window.canData = canData
-window.Render = Render;
-window.Vector2 = Vector2;
-window.WorldObject = WorldObject;
 const a = new WorldObject(
   new Vector2(0, 0),
   new Vector2(250, 250),
   new Vector2(8, 8),
   1);
 window.a = a;
-const p = new WorldObject(
-  new Vector2(0, 0),
-  new Vector2(20, 20),
-  new Vector2(1, 1),
-  0);
+const p = {
+  position: new Vector2(0, 0),
+  velocity: new Vector2(0, 0)
+}
 window.p = p;
 
 //Order objects should be rendered
-var renderList = [a, p]
+var renderList = [a]
 window.currentMove = p;
 window.currentEdit = a;
 
@@ -57,7 +59,7 @@ window.gameStep = function () {
     else if (keyMove.has('d')) { frameMove.x = 1 }
     if (frameMove.length() != 0) {
       frameMove.normalize(true);
-      window.currentMove.position.add(frameMove.multiplyScalar(window.speed), true);
+      window.currentMove.velocity = frameMove.multiplyScalar(window.speed);
     }
   }
   keyRemove.forEach((key) => { keyMove.delete(key) })
@@ -66,6 +68,11 @@ window.gameStep = function () {
   //Clear canvas then redraw
   Render.drawBox(new Vector2(0, 0), new Vector2(500, 500), 'white')
   renderList.forEach((object) => { object.Render() })
+  Render.drawPoint(p.position, 'cyan');
+  Physics.physFunction(p.position, p.velocity, a)
+
+  window.currentMove.position.add(window.currentMove.velocity, true);
+  window.currentMove.velocity.assign(0, 0)
 }
 window.gameSpeed = function (fps) {
   clearInterval(gameID);
